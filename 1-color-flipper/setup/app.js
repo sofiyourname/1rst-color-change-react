3b9b90135b886529b2ae184444925e912cfb9edc
faier <abril.pinneiro@gmail.com>
2021-03-01 18:00:57 -0300

    Commit no anda npm
    no anda man

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -1,1 +1,14 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+btn.addEventListener('Click', function() {
+        //get ramdom number between 0 and number 3 colors[3]
+        const randomNumber = getRandomNumber;
+        document.body.style.backgroundColor = colors[randomNumber];
+        color.textContent = colors[randomNumber];
+        console.log(colors);
+    });
+
+    function getRandomNumber() {
+        return Math.random();
+    }
